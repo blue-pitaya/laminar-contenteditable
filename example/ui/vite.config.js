d22ef1d22837acd3a1030d4d325cefde3b68a803
blue-pitaya <blue-pitaya@users.noreply.github.com>
2023-04-27 00:47:05 +0000

    deploy: 0846537aac08db5907656229d098a6c6b0d580c1

--- example/ui/vite.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "", //needed for github pages to resolve assets correctly
-});
--- project/plugins.sbt
+++
@@ -1,3 +0,0 @@
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.13.0")
-
-libraryDependencies += "org.scala-js" %% "scalajs-env-selenium" % "1.1.1"
